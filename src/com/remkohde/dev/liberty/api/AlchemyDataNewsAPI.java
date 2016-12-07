5f6ffc02fd237c935c14ff5727568494f7592511
Kristian Szoke <Kristian_Szoke@swissre.com>
2016-12-07 08:24:34 -0600

    fixed imports

--- src/com/remkohde/dev/liberty/api/AlchemyDataNewsAPI.java
+++ src/com/remkohde/dev/liberty/api/AlchemyDataNewsAPI.java
@@ -16,12 +16,12 @@ import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 @Path("/watson/news")
 /**
